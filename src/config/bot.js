32b7c5d6d5501354f3220db82364f15026167ffa
vyntrizcpm-commits <vyntrizcpm@gmail.com>
2026-05-13 10:27:48 +0530

    Update bot activity name and type

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,9 +25,10 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "🌸 Keeping things smooth & simple
+",
         // Activity type number (0 = Playing).
-        type: 0, 
+        type: 3, 
       },
     ],
   },
